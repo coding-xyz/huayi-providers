9edc30ea9afca777c6d38e9f630088ed905eaf89
coding-xyz <109228670+coding-xyz@users.noreply.github.com>
2024-01-13 17:36:21 +0800

    fix __init__.py

--- fake_Huayi35/__init__.py
+++ fake_Huayi35/__init__.py
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
 from .fake_Huayi35 import FakeHuayi35
 from .fake_Huayi35 import FakeHuayi35V2
-=======
-from .fake_Huayi35 import FakeHuayi35
-from .fake_Huayi35 import FakeHuayi35V2
->>>>>>> e137d03f7dbbc2f0b2425aade99617c8b7b784a7
