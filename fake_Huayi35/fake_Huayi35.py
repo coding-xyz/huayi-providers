9b1883c5455b6e47fbb276e2a4913e8af392b1dc
coding-xyz <109228670+coding-xyz@users.noreply.github.com>
2024-01-14 13:36:26 +0800

    clean-up fake_Huayi35

--- fake_Huayi35/fake_Huayi35.py
+++ fake_Huayi35/fake_Huayi35.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import os
 from qiskit.providers.fake_provider import fake_qasm_backend, fake_backend
 
@@ -13,19 +12,3 @@ class FakeHuayi35V2(fake_backend.FakeBackendV2):
 	conf_filename = 'conf_Huayi35.json'
 	props_filename = 'props_Huayi35.json'
 	backend_name = 'fake_Huayi35'
-=======
-import os
-from qiskit.providers.fake_provider import fake_qasm_backend, fake_backend
-
-class FakeHuayi35(fake_qasm_backend.FakeQasmBackend):
-	dirname = os.path.dirname(__file__)
-	conf_filename = 'conf_Huayi35.json'
-	props_filename = 'props_Huayi35.json'
-	backend_name = 'fake_Huayi35'
-
-class FakeHuayi35V2(fake_backend.FakeBackendV2):
-	dirname = os.path.dirname(__file__)
-	conf_filename = 'conf_Huayi35.json'
-	props_filename = 'props_Huayi35.json'
-	backend_name = 'fake_Huayi35'
->>>>>>> e137d03f7dbbc2f0b2425aade99617c8b7b784a7
